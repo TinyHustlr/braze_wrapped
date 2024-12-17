5b060cc76aa4768aaa3c7ac25a3fb79616a90085
David O'reilly <davidworeilly@gmail.com>
2024-12-17 20:17:16 +1100

    init commit

---
+++ Html_JS/brazeWrapped.js
@@ -0,0 +1,19 @@
+const wrappedDialog = document.getElementById("wrapped-dialog")
+const closeButton = document.getElementById("close-btn")
+
+// opens modal
+wrappedDialog.showModal()
+
+// start siema carousel
+const mySiema = new Siema({
+  duration: 500,
+  loop: true,
+});
+
+// listen for keydown event
+setInterval(() => mySiema.next(), 6000)|            
+
+// close modal function
+closeButton.addEventListener("click", ()=> {
+    brazeBridge.closeMessage()
+})
